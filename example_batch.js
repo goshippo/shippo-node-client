973c28a8b8956cf2536bb0d848c6c1a7b490e3ff
Guozhen La <guozhenla@guozhen-shippo-mbp.local>
2017-01-18 14:47:45 -0800

    removed hardcoded objects from batch example

--- example_batch.js
+++ example_batch.js
@@ -5,14 +5,15 @@ as well as adding and removing shipments from a batch
 
 
 // replace <YOUR_PRIVATE_KEY> with your ShippoToken key
-var shippo = require('shippo')('<YOUR_SHIPPO_KEY>');
+var shippo = require('shippo')('<YOUR_PRIVATE_KEY>');
 
 //Maximum time we'll wait for batch to clear
 const BATCH_WAIT_TIMEOUT = 10000;
+const POLLING_INTERVAL = 1000;
 
 // example Batch object for batch shipment creation
 var myBatch = {
-  "default_carrier_account": "e68e95b95e33431a87bdecdd2b891c2b",
+  "default_carrier_account": "<YOUR_USPS_ACCOUNT_OBJECT_ID>",
   "default_servicelevel_token": "usps_priority",
   "label_filetype": "PDF_4x6",
   "metadata": "BATCH #170",
@@ -95,64 +96,100 @@ var myBatch = {
   ]
 }
 
-var myBatchId = '3383a45c840b4754b8c26652f88213d3'
+var addressFrom  = {
+  "object_purpose":"PURCHASE",
+  "name":"Ms Hippo",
+  "company":"Shippo",
+  "street1":"215 Clayton St.",
+  "city":"San Francisco",
+  "state":"CA",
+  "zip":"94117",
+  "country":"US", //iso2 country code
+  "phone":"+1 555 341 9393",
+  "email":"ms-hippo@goshippo.com",
+}
 
-// example Array of shipments to add to a batch shipment
-var shipmentsToAdd = [
-    {"shipment": "ad1e998b77d04c6a98a9d35549f4b4e9"},
-    {"shipment": "e5c74868fc974785889b4b5ab6c9b635"}
-]
+// example address_to object dict
+var addressTo = {
+  "object_purpose":"PURCHASE",
+  "name":"Mr Hippo",
+  "company":"London Zoo",
+  "street1":"Regent's Park",
+  "street2":"Outer Cir",
+  "city":"LONDON",
+  "state":"",
+  "zip":"NW1 4RY",
+  "country":"GB", //iso2 country code
+  "phone":"+1 555 341 9393",
+  "email":"mrhippo@goshippo.com",
+  "metadata" : "Hippo T-Shirt Order #1043"
+}
 
-// example Array of shipments to remove from a batch
-var shipmentsToRemove = [
-    "1098c49d6e304f45a030e730155c97f0",
-    "5766ef14ee224d1db794cc072f746724"
-]
+// parcel object dict
+var parcel = {
+  "length":"5",
+  "width":"5",
+  "height":"5",
+  "distance_unit":"in",
+  "weight":"2",
+  "mass_unit":"lb",
+}
 
-// example of adding shipments to a batch
-var anotherBatchId = "d1fd274139364202bf87c02030d73d8b"
+var shipments = []
 
-// example of creating a batch shipment using a Batch object
-shippo.batch.create(myBatch)
-.then(function(createResponse) {
+shippo.shipment.create({
+  "object_purpose": "PURCHASE",
+  "address_from": addressFrom,
+  "address_to": addressTo,
+  "parcel": parcel,
+  "async": false
+}).then(function(shipmentResponse) {
+  console.log("Shipment created with object id: %s", shipmentResponse.object_id);
+  shipments.push({"shipment" : shipmentResponse.object_id});
+  // example of creating a batch shipment
+  shippo.batch.create(myBatch)
+  .then(function(createResponse) {
     console.log("Batch shipment creation response: %s", JSON.stringify(createResponse, null, 4));
-    // example of retrieving a previously created batch shipment
+    //Poll a batch object to check for a VALID status before adding/removing shipments or purchasing
     checkBatchStatus(createResponse.object_id);
-}, function(createErr) {
-    console.log("There was an error creating the batch shipment: %s", createErr);
+  }, function(createErr) {
+      console.log("There was an error creating the batch shipment: %s", createErr);
+  });
+}, function(shipmentErr) {
+  console.log("There was an error creating a shipment: %s", shipmentErr);
 });
 
-
-
-shippo.batch.add(anotherBatchId, shipmentsToAdd)
-.then(function(addResponse) {
-    console.log("Response from adding shipments to batch: %s", JSON.stringify(addResponse, null, 4));
-
-    // example of removing shipments from a batch
-    shippo.batch.remove(anotherBatchId, shipmentsToRemove)
-    .then(function(removeResponse) {
-        console.log("Response from removing shipments from the batch: %s", JSON.stringify(removeResponse, null, 4));
-    }, function(removeErr) {
-        console.log("There was an error removing shipments from the batch: %s", removeErr);
-    });
-}, function(addErr) {
-    console.log("There was an error adding shipments to the batch: %s", addErr);
-});
-
+//Using Batch::retrieve to poll for the batch's VALID status is done for demo purposes only
+//In practice, it is recommended to register a Batch Create webhook for status updates 
 var timeout = 0;
 function checkBatchStatus(object_id) {
   shippo.batch.retrieve(object_id)
   .then(function(response) {
     if (response.object_status === "VALID") {
-      shippo.batch.purchase(response.object_id)
-        .then(function(purchaseResponse) {
-            console.log("Batch shipment purchase response: %s", JSON.stringify(purchaseResponse, null, 4));
-        }, function(purchaseErr) {
-            console.log("There was an error purchasing the batch shipment: %s", purchaseErr);
+      //Example of adding a shipment to a batch object
+      shippo.batch.add(response.object_id, shipments)
+      .then(function(addResponse) {
+        console.log("Response from adding shipments to batch: %s", JSON.stringify(addResponse, null, 4));
+         //Example of removing a shipment from a batch object
+        shippo.batch.remove(addResponse.object_id, [addResponse.batch_shipments.results[0].object_id])
+        .then(function(removeResponse) {
+          console.log("Response from removing shipments from the batch: %s", JSON.stringify(removeResponse, null, 4));
+          //Example of purchasing a batch shipment
+          shippo.batch.purchase(response.object_id)
+          .then(function(purchaseResponse) {
+              console.log("Batch shipment purchase response: %s", JSON.stringify(purchaseResponse, null, 4));
+          }, function(purchaseErr) {
+              console.log("There was an error purchasing the batch shipment: %s", purchaseErr);
+          });
+        }, function(removeErr) {
+          console.log("There was an error removing shipments from the batch: %s", removeErr);
         });
+      }, function(addErr) {
+        console.log("There was an error adding shipments to the batch: %s", addErr);
+      });      
     } else if (timeout < BATCH_WAIT_TIMEOUT){
-      timeout = timeout + 1000;
-      setTimeout(checkBatchStatus, 1000, response.object_id);
+      timeout = timeout + POLLING_INTERVAL;
+      setTimeout(checkBatchStatus, POLLING_INTERVAL, response.object_id);
     } else{
       console.log("Batch purchase timed out on batch %s", response.object_id);
     }
