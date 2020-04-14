01572d1a7c236ddb994a0c3edf24c5fef772653c
Jonathan Campbell <jcampbell@goshippo.com>
2020-04-14 14:56:52 -0700

    populate more attributes on Error object

--- test/Error.spec.js
+++ test/Error.spec.js
@@ -14,10 +14,20 @@ describe('Error', function() {
   });
 
   describe('ShippoError', function() {
+
     it('Generates specific instance depending on error-type', function() {
       expect(Error.ShippoError.generate({ type: 'invalid_request_error' })).to.be.instanceOf(Error.ShippoInvalidRequestError);
       expect(Error.ShippoError.generate({ type: 'api_error' })).to.be.instanceOf(Error.ShippoAPIError);
     });
+
+    it('Populates named parameters', function() {
+      var fields = { code: 400, detail: { stuff: 'is bad' }, message: 'bad stuff', path: '/badpath/' };
+      var e = new Error.ShippoAPIError(fields);
+      for (var k in Object.keys(fields)) {
+        expect(e[k]).to.eql(fields[k]);
+      }
+    })
+
   });
 
 });
